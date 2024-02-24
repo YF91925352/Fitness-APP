4f7bb771d143eed1845001d989811c356a0775bf
YF91925352 <zyf91925352@outlook.com>
2024-02-24 10:14:37 +0800

    add motion to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,6 +7,7 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import { motion } from "framer-motion";
 
 interface Props {
   setSelectedPage: (value: SelectedPage) => void;
@@ -17,11 +18,24 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className=" bg-gray-20 py-8  md:h-full md:pb-0">
       {/* Image and main header */}
-      <div className="md:h-6/7 mx-auto w-5/6 items-center  justify-center md:flex ">
+      <motion.div
+        className="md:h-6/7 mx-auto w-5/6 items-center  justify-center md:flex "
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
         {/* Main Header */}
         <div className="mt-20  md:basis-3/5">
           {/* Headings */}
-          <div className="flex flex-col items-center gap-8">
+          <motion.div
+            className="flex flex-col items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.8 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="md:-mb-9">
               <img alt="home-page-text" src={HomePageText} />
             </div>
@@ -29,10 +43,20 @@ const Home = ({ setSelectedPage }: Props) => {
               Unrivaled Gym ! Unparalleled Training Fitness Classes ! Get Your
               Dream Body Now.
             </p>
-          </div>
+          </motion.div>
 
           {/* Actions */}
-          <div className="flex items-center gap-12">
+          <motion.div
+            className="flex items-center gap-12"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.8 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -43,14 +67,14 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/* Images */}
         <div className=" md: flex basis-3/5 md:ml-36 md:mt-16 md:mb-2 md:justify-items-end">
           <img src={HomePageGraphic} alt="home-page-grphic" />
         </div>
-      </div>
+      </motion.div>
       {/* Sponsors */}
       {isAboveMediumScreen && (
         <div className=" w-full bg-primary-100 py-4">
